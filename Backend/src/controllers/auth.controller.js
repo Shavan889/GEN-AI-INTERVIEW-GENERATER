099928dd8c273568c579331bb20ae1b2019d9952
Shavan889 <shavansanhotra@gmail.com>
2026-05-29 12:27:24 +0530

    fix production cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -98,7 +98,12 @@ async function loginUserController(req, res) {
     },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
 
   res.status(200).json({
     message: "User loggedIn successfully",
@@ -121,7 +126,11 @@ async function logoutUserController(req, res) {
   if (token) {
     await tokenBlacklistModel.create({ token });
 
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
   }
 
   res.status(200).json({
