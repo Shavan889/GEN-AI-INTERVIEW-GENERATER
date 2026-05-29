84e651d92a89c1cb332a82fd4a162e922ed35e3e
Shavan889 <shavansanhotra@gmail.com>
2026-05-29 12:01:04 +0530

    add vercel url to cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://gen-ai-interview-generater.vercel.app",
+    ],
     credentials: true,
   }),
 );
